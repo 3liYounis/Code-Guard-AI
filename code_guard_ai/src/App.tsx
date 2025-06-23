e87e5b13f87e09056fd0ad8f65ac676a25a1195a
3liyounis <aliyounis642@gmail.com>
2025-06-23 16:01:25 +0300

    Integrate Chakra UI

--- code_guard_ai/src/App.tsx
+++ code_guard_ai/src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { HStack, Skeleton, SkeletonCircle, Stack } from '@chakra-ui/react'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HStack gap="5" justifyContent="center" alignItems="center" padding={10}>
+      <SkeletonCircle size="12" />
+      <Stack flex="1">
+        <Skeleton height="5" />
+        <Skeleton height="5" width="80%" />
+      </Stack>
+    </HStack>
   )
 }
-
-export default App
+export default App;
