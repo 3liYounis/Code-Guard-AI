5d1fd8a1c3f506e928c214f8c2ad42364d32cda7
3liyounis <aliyounis642@gmail.com>
2025-06-23 22:52:45 +0300

    Add A Shared App.tsx For Testing

---
+++ code_guard_ai/src/App.tsx
@@ -0,0 +1,25 @@
+import ReviewCard, { type CodeReview } from './components/Review Card/ReviewCard';
+import AuthenticationCard from './components/Authentication/AuthenticationCard';
+import './App.css'
+const codeReview: CodeReview = {
+  name: 'Behavioral Analysis',
+  programmingLanguage: "Python",
+  security: 90,
+  cleanliness: 52,
+  maintainability: 95,
+  recommendations: [
+    { id: 1, type: "security", content: "If anyone thinks he is something when he is nothing, he deceives himself. Each one should test his own actions. Then he can take pride in himself, without comparing himself to anyone else.", cite: "Chat GPT" },
+    { id: 2, type: "maintainability", content: "one two three four one two three four one two three four", cite: "SE Best Practices" },
+    { id: 3, type: "cleanliness", content: "one two three four one two three four one two three four", cite: "SOC Principles" },
+  ]
+}
+function App() {
+
+  return (
+    <>
+      <ReviewCard codeReview={codeReview} />
+      <AuthenticationCard />
+    </>
+  );
+}
+export default App;
