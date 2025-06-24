eb89b55d1d124a65b8cb3892ec3bc9eb900847ba
3liyounis <aliyounis642@gmail.com>
2025-06-25 00:05:07 +0300

    Add A File Emoji

--- code_guard_ai/src/components/ui/Emoji.tsx
+++ code_guard_ai/src/components/ui/Emoji.tsx
@@ -3,15 +3,17 @@ import security_lock from "../../assets/security_lock.png"
 import clean from "../../assets/clean.png"
 import maintain from "../../assets/maintain.png"
 import bulb from "../../assets/bulb.png"
+import file from "../../assets/file.png"
 interface Props {
     type: string;
 }
 const Emoji = ({ type }: Props) => {
     const emojiMap: { [key: string]: ImageProps } = {
-        "security": { src: security_lock, alt: 'security', boxSize: "20px" },
-        "cleanliness": { src: clean, alt: 'cleanliness', boxSize: "20px" },
-        "maintainability": { src: maintain, alt: 'maintainability', boxSize: "20px" },
-        "other": { src: bulb, alt: 'maintainability', boxSize: "25px", marginTop: 0 },
+        "Security": { src: security_lock, alt: 'security', boxSize: "20px" },
+        "Cleanliness": { src: clean, alt: 'cleanliness', boxSize: "20px" },
+        "Maintainability": { src: maintain, alt: 'maintainability', boxSize: "20px" },
+        "File": { src: file, alt: 'file', boxSize: "160px" },
+        "other": { src: bulb, alt: 'other', boxSize: "25px", marginTop: 0 },
     }
     return (
         <Image marginTop={2} {...emojiMap[type]} />
