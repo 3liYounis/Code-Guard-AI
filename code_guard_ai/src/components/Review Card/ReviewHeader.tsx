535ba4ca18a811fe667f37bac07b1cfefdaab6b3
3liyounis <aliyounis642@gmail.com>
2025-06-24 22:43:14 +0300

    Add More Programming Languages Icons

--- code_guard_ai/src/components/Review Card/ReviewHeader.tsx
+++ code_guard_ai/src/components/Review Card/ReviewHeader.tsx
@@ -9,18 +9,27 @@ import {
 import type { IconType } from "react-icons";
 import { FaPython, FaJava } from "react-icons/fa6";
 import { SiTypescript } from "react-icons/si";
-import { DiJsBadge } from "react-icons/di";
-import { TbBrandCSharp } from "react-icons/tb";
+import { DiJsBadge, DiRubyRough } from "react-icons/di";
+import { TbBrandCSharp, TbBrandKotlin } from "react-icons/tb";
+import { GrStatusUnknown } from "react-icons/gr";
+import { IoLogoReact } from "react-icons/io5";
+import { FaVuejs } from "react-icons/fa";
+
 interface Props {
   name: string;
   programmingLanguage: string;
 }
 const mapLanguage: { [key: string]: IconType } = {
-  Java: FaJava,
+  "Java": FaJava,
   "C#": TbBrandCSharp,
-  Python: FaPython,
-  JavaScript: DiJsBadge,
-  TypeScript: SiTypescript,
+  "Python": FaPython,
+  "JavaScript": DiJsBadge,
+  "TypeScript": SiTypescript,
+  "Ruby": DiRubyRough,
+  "Kotlin": TbBrandKotlin,
+  "React": IoLogoReact,
+  "Vue.js": FaVuejs,
+  "other": GrStatusUnknown
 };
 const ringCss = defineStyle({
   outlineWidth: "5px",
@@ -29,10 +38,11 @@ const ringCss = defineStyle({
   outlineStyle: "solid",
 });
 const ReviewHeader = ({ name, programmingLanguage }: Props) => {
+  const iconSource = mapLanguage[programmingLanguage] ?? mapLanguage["other"];
   return (
     <HStack>
       <Avatar.Root css={ringCss}>
-        <Icon as={mapLanguage[programmingLanguage]} boxSize={6} />
+        <Icon as={iconSource} boxSize={6} />
       </Avatar.Root>
       <Stack gap={0} padding={3}>
         <Text fontWeight="medium">{name}</Text>
