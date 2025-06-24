2da4a965ff19c0457a2cd82340803e7a16faa0ec
3liyounis <aliyounis642@gmail.com>
2025-06-24 22:34:39 +0300

    Remove Old Code

--- code_guard_ai/src/components/Review Card/AnimatedBox.tsx
+++ code_guard_ai/src/components/Review Card/AnimatedBox.tsx
@@ -1,66 +1,8 @@
-// import { useState, type ReactNode } from "react";
-// import { Box, Button, Flex } from "@chakra-ui/react";
-// import { motion } from "framer-motion";
-
-// const MotionBox = motion.create(Box);
-
-// interface Props {
-//     children: ReactNode;
-//     width: number;
-//     height: number;
-// frontText: string;
-// backText: string;
-// }
-// const AnimatedBox = ({ children, width, height, frontText, backText }: Props) => {
-//     const [flipped, setFlipped] = useState(false);
-//     return (
-//         <MotionBox
-//             initial={{ opacity: 0, scale: 0.95 }}
-//             animate={{ opacity: 1, scale: 1 }}
-//             transition={{ duration: 0.4 }}
-//         >
-//             {/* <Flex
-//                 width={width}
-//                 height={height}
-//                 justify="center"
-//                 align="center"
-//                 py={8}
-//                 cursor="pointer"
-//             > */}
-//             <Box perspective="3000px" width={width} height={height + 40}>
-//                 <MotionBox
-//                     position="relative"
-//                     width="100%"
-//                     height="100%"
-//                     animate={{ rotateY: flipped ? 180 : 0 }}
-//                     transition={{ duration: 0.6 }}
-//                     style={{ transformStyle: "preserve-3d" }}
-//                 >
-//                     {children}
-//                 </MotionBox>
-//                 <Button
-//                     position="absolute"
-//                     bottom="10px"
-//                     left="50%"
-//                     transform="translateX(-50%)"
-//                     marginBottom={3}
-//                     onClick={() => setFlipped((flipped) => !flipped)}
-//                 >
-//                     {flipped ? backText : frontText}
-//                 </Button>
-//             </Box>
-//             {/* </Flex> */}
-//         </MotionBox >
-//     );
-// };
-
-// export default AnimatedBox;
 import { Children, type ReactNode } from "react";
 import { Box, Button } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { useState } from "react";
 const MotionBox = motion.create(Box);
-
 interface Props {
     width: number;
     height: number;
@@ -68,7 +10,6 @@ interface Props {
     backText: string;
     children: ReactNode;
 }
-
 const AnimatedBox = ({ children, width, height, frontText, backText }: Props) => {
     const [flipped, setFlipped] = useState(false);
     const [front, back] = Children.toArray(children).slice(0, 2);
@@ -80,7 +21,6 @@ const AnimatedBox = ({ children, width, height, frontText, backText }: Props) =>
                 width={width}
                 height={height + 30}
                 position="relative">
-
                 <MotionBox
                     position="relative"
                     width="100%"
@@ -112,7 +52,6 @@ const AnimatedBox = ({ children, width, height, frontText, backText }: Props) =>
                         {back}
                     </Box>
                 </MotionBox>
-
                 <Button
                     position="absolute"
                     bottom="10px"
@@ -126,5 +65,4 @@ const AnimatedBox = ({ children, width, height, frontText, backText }: Props) =>
         </MotionBox>
     );
 };
-
 export default AnimatedBox;
