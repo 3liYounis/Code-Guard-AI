c5643f6871090843834bbc8fbb11ff17ef46f00c
3liyounis <aliyounis642@gmail.com>
2025-06-25 21:48:37 +0300

    Add Tooltip For Each Card Button

--- code_guard_ai/src/components/Dashboard/Review Card/Buttons/CardButton.tsx
+++ code_guard_ai/src/components/Dashboard/Review Card/Buttons/CardButton.tsx
@@ -1,15 +1,30 @@
 import { Button } from '@chakra-ui/react';
+import { Tooltip } from "@/components/ui/tooltip"
 import Emoji from '@/components/ui/Emoji';
+
 interface Props {
     type: string;
     onClicked: () => void;
 }
+const cssMap: { [key: string]: {} } = {
+    "Delete": { "--tooltip-bg": "colors.red.500" },
+    "Edit": { "--tooltip-bg": "colors.yellow.500" },
+    "View": { "--tooltip-bg": "colors.green.500" }
+}
 const CardButton = ({ type, onClicked }: Props) => {
     return (
-        <Button onClick={onClicked} height={5}>
-            <Emoji type={type} />
-        </Button>
+        <Tooltip
+            showArrow
+            content={type}
+            contentProps={{ css: { ...cssMap[type], color: "ActiveBorder" } }}
+            positioning={{ offset: { mainAxis: 2, crossAxis: 2 }, placement: "left-end" }}
+            openDelay={500}
+            closeDelay={100}
+        >
+            <Button onClick={onClicked} height={5}>
+                <Emoji type={type} />
+            </Button>
+        </Tooltip>
     )
 }
-
-export default CardButton
+export default CardButton;
