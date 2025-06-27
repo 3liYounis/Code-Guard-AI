2c378185829be81a57ce0dc917f90a12bc9c233b
3liyounis <aliyounis642@gmail.com>
2025-06-27 20:10:09 +0300

    Style: Profile Icon and its buttons

--- code_guard_ai/src/components/Authentication Cards/ProfileIcon.tsx
+++ code_guard_ai/src/components/Authentication Cards/ProfileIcon.tsx
@@ -1,7 +1,9 @@
 "use client";
 import type { User } from "../../services/FirebaseManager";
-import { Menu, Portal, Box, Text, Icon, Button } from "@chakra-ui/react";
-import { FaUser } from "react-icons/fa";
+import { Menu, Portal, Box, Text, Icon, Button, Flex, HStack } from "@chakra-ui/react";
+import { MdAccountCircle } from "react-icons/md";
+import { BiLogOut } from "react-icons/bi";
+
 
 interface ProfileIconProps {
   user: User | undefined;
@@ -12,24 +14,29 @@ export default function ProfileIcon({ user, onSignOut }: ProfileIconProps) {
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
-        <Button aria-label="User menu" variant="ghost" size="sm">
-          {<Icon as={FaUser} boxSize={6} />}
+        <Button aria-label="User menu" variant="ghost" p={0} minW="auto" borderRadius="full" height="auto">
+          {<Icon as={MdAccountCircle} boxSize={12} color="ActiveBorder" />}
         </Button>
       </Menu.Trigger>
 
       <Portal>
         <Menu.Positioner>
           <Menu.Content minW="xs" py={0}>
-            <Box px={4} py={3}>
+            <Box px={4} py={2} mt={1}>
               <Text fontWeight="bold">Hello {user?.displayName}!</Text>
               <Text fontSize="sm" color="gray.500">
                 {user?.email}
               </Text>
             </Box>
             <Menu.Separator />
-            <Button mb={2} onClick={onSignOut}>
-              Sign Out
-            </Button>
+            <Flex justify="center">
+              <Button onClick={onSignOut} mb={1} width="80%">
+                <HStack>
+                  <Icon as={BiLogOut} boxSize={6} />
+                  <Text fontFamily="cursive">Log Out</Text>
+                </HStack>
+              </Button>
+            </Flex>
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
