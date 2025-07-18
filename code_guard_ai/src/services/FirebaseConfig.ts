b9ceb21ae8b01b677dfb2ec240bf042c91fbfef1
3liyounis <aliyounis642@gmail.com>
2025-07-19 00:59:40 +0300

    Stop tracking Firebase Config file

--- code_guard_ai/src/services/FirebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCgK-W9iHRzjfw25NJ_zPbda0asBEuECR0",
-    authDomain: "code-guard-ai.firebaseapp.com",
-    projectId: "code-guard-ai",
-    storageBucket: "code-guard-ai.firebasestorage.app",
-    messagingSenderId: "184582675821",
-    appId: "1:184582675821:web:2127223dc762cf07188e64",
-    measurementId: "G-CGTVGVWVXB"
-};
